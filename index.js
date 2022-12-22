4398779f39f2aff8d73eedd00737e939dd918d0c
SalamanderCtesiphon <swbrookshire@gmail.com>
2022-12-22 17:58:59 -0600

    still attempting to extract the temperature from the api response

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 const container = document.getElementById('container');
 const weather = document.querySelector('p');
 
-fetch('https://api.openweathermap.org/data/2.5/weather?q=Dallas&appid=9931662c9d29a785239ccf3724e38c86&s=temp', {mode: 'cors'})
+
+fetch('https://api.openweathermap.org/data/2.5/weather?lat=31.997346&lon=-102.077915&appid=9931662c9d29a785239ccf3724e38c86&units=imperial', {mode: 'cors'})
     .then(function(response) {
         return response.json();
     })
-    .then(function(data) {
-        console.log(data);
+    .then(function(temp) {
+        console.log(temp);
     });
