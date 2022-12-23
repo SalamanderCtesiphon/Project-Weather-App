4349a2169c890477efd1d246f3ff1d1ad3086705
SalamanderCtesiphon <swbrookshire@gmail.com>
2022-12-22 22:31:29 -0600

    input the html

--- index.js
+++ index.js
@@ -1,20 +1,3 @@
-const container = document.getElementById('container');
-const weather = document.querySelector('p');
-
-
-fetch('https://api.openweathermap.org/data/2.5/weather?lat=31.997346&lon=-102.077915&appid=&units=imperial', {mode: 'cors'})
-    .then(function(response) {
-        return response.json();
-    })
-    .then(function(response) {
-        weather.textContent = "Current Midland Teperature: " + response.main.temp + '°F';
-    });
-
-
-
-const city = document.getElementById('city');
-const submit = document.getElementById('submit');
-
-
+//appid=9931662c9d29a785239ccf3724e38c86
 
 
