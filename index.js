38630bb5f18bf6f2efe0510d2e34dc938a115f29
SalamanderCtesiphon <swbrookshire@gmail.com>
2022-12-22 19:22:59 -0600

    removed my web api key from the code

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const container = document.getElementById('container');
 const weather = document.querySelector('p');
 
 
-fetch('https://api.openweathermap.org/data/2.5/weather?lat=31.997346&lon=-102.077915&appid=9931662c9d29a785239ccf3724e38c86&units=imperial', {mode: 'cors'})
+fetch('https://api.openweathermap.org/data/2.5/weather?lat=31.997346&lon=-102.077915&appid=&units=imperial', {mode: 'cors'})
     .then(function(response) {
         return response.json();
     })
